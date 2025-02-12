83e824648e40663e25b491db9b756e4c3cae2b68
Josh Schmelzle <josh@joshschmelzle.com>
2025-02-11 18:10:40 -0600

    simplify intended use motd

--- wlanpi1-lite/06-sys-conf/02-run.sh
+++ wlanpi1-lite/06-sys-conf/02-run.sh
@@ -58,15 +58,7 @@ fi
 
 # --- MOTD configuration ---
 
-echo "Welcome to WLAN Pi OS!
-
-* This device is for educational, lab, or testing use ONLY.
-* Permanent installs, continuous transmission, & commercial use are PROHIBITED.
-* NO WARRANTY, express or implied.
-* You are solely responsible for complying with all laws and for the consequences of any use. 
-
-By using this device, you agree to these terms.
-" > /etc/motd
+echo "Welcome to WLAN Pi OS. This device is intended for educational, laboratory, and non-commercial testing purposes. WLAN Pi provides no warranty, express or implied. You are solely responsible for complying with applicable laws and regulations." > /etc/motd
 
 echo "=== Completed wlanpi-system-init $(date) ===" ' > /usr/sbin/wlanpi-system-init
 
