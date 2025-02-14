960e69c0c03f88e26ff2ab4722c8f6b5259d8752
Josh Schmelzle <josh@joshschmelzle.com>
2025-02-14 16:14:08 -0500

    fix first boot

--- wlanpi0-base/10-first-boot/files/usr/bin/first-boot.sh
+++ wlanpi0-base/10-first-boot/files/usr/bin/first-boot.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+set -e 
+
+if ! command -v ufw >/dev/null 2>&1; then
+    echo "ufw not found ..."
+fi
+
 ufw limit ssh
-ufw enable
+ufw --force enable
+wlanpi-reg-domain set US
+
+exit 0
--- wlanpi1-lite/08-set-reg/00-set-reg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-# TODO remove this and migrate regulatory out of pi-gen
-wlanpi-reg-domain set US
-EOF
