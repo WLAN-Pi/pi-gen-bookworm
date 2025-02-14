dcfef542a42ba4fd86c88819fcc4b1c5ff8102e3
Josh Schmelzle <josh@joshschmelzle.com>
2025-02-14 17:09:39 -0500

    move stage script to correct location

--- wlanpi0-base/10-first-boot/00-run.sh
+++ wlanpi0-base/10-first-boot/00-run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
 # Setup first boot script
+echo "Copying first-boot.sh ..."
 copy_overlay /usr/bin/first-boot.sh -o root -g root -m 755
+
+echo "Copying wlanpi-first-boot service file ..."
 copy_overlay /lib/systemd/system/wlanpi-first-boot.service -o root -g root -m 644
 
+echo "Enabling wlanpi-first-boot service ..."
 on_chroot <<CHEOF
 	systemctl enable wlanpi-first-boot
 CHEOF
