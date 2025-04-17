88deec01d9183390cefa04d00d53f9f8caaf650b
jsz <josh@joshschmelzle.com>
2025-04-17 00:06:00 -0400

    don't need dup services for first boot things.

--- wlanpi1-lite/10-first-boot/files/usr/bin/first-boot.sh
+++ wlanpi1-lite/10-first-boot/files/usr/bin/first-boot.sh
@@ -10,4 +10,6 @@ ufw limit ssh
 # ufw --force enable
 raspi-config nonint do_wifi_country US
 
+echo "Welcome to WLAN Pi OS. This device is intended for educational, laboratory, and non-commercial testing purposes. WLAN Pi provides no warranty, express or implied. You are solely responsible for complying with applicable laws and regulations." > /etc/motd
+
 exit 0
