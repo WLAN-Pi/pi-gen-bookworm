93e2efe2d4f262ae55910d5d904761f7657bdcd4
Josh Schmelzle <josh@joshschmelzle.com>
2025-04-18 22:04:18 -0400

    tsharksss

--- wlanpi1-lite/07-install-packages/00-run.sh
+++ wlanpi1-lite/07-install-packages/00-run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-echo "=== tshark, netperf, fping, irtt install ==="
+echo "=== tshark install ==="
 # Set tshark/wireshark preseed
 echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-set-selections
 
 # Install tshark
 apt install -y tshark 
-
-apt install -y netperf fping irtt
-apt install -y --no-install-recommends flent
-echo "=== tshark, netperf, fping, irtt done ==="
+echo "=== tshark done ==="
 EOF
