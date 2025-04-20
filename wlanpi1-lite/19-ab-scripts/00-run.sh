d47a63e733392df0f49a479141393895b8966901
jsz <josh@joshschmelzle.com>
2025-04-20 00:15:38 -0400

    Fix run.sh for AB scripts (#29)
    * Fix run.sh for ab scripts

--- wlanpi1-lite/19-ab-scripts/00-run.sh
+++ wlanpi1-lite/19-ab-scripts/00-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
-install -v -m 755 files/commit_current_partition "${ROOTFS_DIR}/usr/local/sbin/"
-install -v -m 755 files/go-serial2json.sh "${ROOTFS_DIR}/usr/local/sbin/"
-install -v -m 755 files/go-device-info-decoder.sh "${ROOTFS_DIR}/usr/local/sbin/"
+install -v -m 755 files/boot-info "${ROOTFS_DIR}/usr/local/sbin/"
+install -v -m 755 files/commit-current-partition "${ROOTFS_DIR}/usr/local/sbin/"
+install -v -m 755 files/switch-partition "${ROOTFS_DIR}/usr/local/sbin/"
+install -v -m 755 files/update-inactive-partition "${ROOTFS_DIR}/usr/local/sbin/"
